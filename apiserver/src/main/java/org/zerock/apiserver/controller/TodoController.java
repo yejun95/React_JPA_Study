151ed27b000e4d2a333ba0498d64ece570ab085d
jun <xodlf100@gmail.com>
2025-01-21 23:49:15 +0900

    feat Backend - add TodoController * apply RestController

---
+++ apiserver/src/main/java/org/zerock/apiserver/controller/TodoController.java
@@ -0,0 +1,35 @@
+package org.zerock.apiserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.apiserver.dto.PageRequestDTO;
+import org.zerock.apiserver.dto.PageResponseDTO;
+import org.zerock.apiserver.dto.TodoDTO;
+import org.zerock.apiserver.service.TodoService;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/api/todo")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @GetMapping("/{tno}")
+    public TodoDTO get(@PathVariable("tno") Long tno) {
+
+        return todoService.get(tno);
+    }
+
+    @GetMapping("/list")
+    public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
+
+        log.info("list============== " + pageRequestDTO);
+
+        return todoService.getList(pageRequestDTO);
+    }
+}
