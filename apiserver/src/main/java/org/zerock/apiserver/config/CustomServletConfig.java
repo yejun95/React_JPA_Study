a4230da9f5e2d0a4a0198cfcba992675dde42fbd
jun <xodlf100@gmail.com>
2025-01-22 23:40:15 +0900

    feat Backend - set up CORS

--- apiserver/src/main/java/org/zerock/apiserver/config/CustomServletConfig.java
+++ apiserver/src/main/java/org/zerock/apiserver/config/CustomServletConfig.java
@@ -3,6 +3,7 @@ package org.zerock.apiserver.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.apiserver.controller.fotmatter.LocalDateFormatter;
 
@@ -14,5 +15,13 @@ public class CustomServletConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         log.info("==========addFormatters===========");
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .maxAge(500)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedOrigins("*");
     }
 }
