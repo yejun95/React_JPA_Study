4dbf2abe05f761210bc4bbd7509179f553aa9c0c
jun <xodlf100@gmail.com>
2025-01-30 19:03:32 +0900

    feat React - add AddComponent

---
+++ mall/src/components/todo/AddComponent.js
@@ -0,0 +1,87 @@
+import React, {useState} from 'react';
+
+const initState = {
+    title: '',
+    content: '',
+    dueDate: ''
+}
+
+function AddComponent(props) {
+    const [todo, setTodo] = useState(initState);
+
+    const handleChangeTodo = (e) => {
+        console.log(e.target.name, e.target.value)
+
+        // todo[title,,, content,,, duedate]
+        todo[e.target.name] = e.target.value;
+
+        setTodo({...todo})
+    }
+
+    const handleClickAdd = () => {
+        console.log('todo', todo)
+    }
+
+    return (
+        <div className={'border-2 border-sky-200 mt-10 m-2 p-4'}>
+
+            {/* TITLE */}
+            <div className={'flex justify-center'}>
+                <div className={'relative mb-4 flex w-full flex-wrap items-stretch'}>
+                    <div className={'w-1/5 p-6 text-right font-bold'}>TITLE</div>
+                    <input className={'w-4/5 p-6 rounded-r borer border-solid border-neutral-500 shadow-md'}
+                           name={'title'}
+                           text={'text'}
+                           value={todo.title}
+                           onChange={handleChangeTodo}
+                    >
+                    </input>
+                </div>
+            </div>
+
+            {/* CONTENT */}
+            <div className={'flex justify-center'}>
+                <div className={'relative mb-4 flex w-full flex-wrap items-stretch'}>
+                    <div className={'w-1/5 p-6 text-right font-bold'}>CONTENT</div>
+                    <input className={'w-4/5 p-6 rounded-r borer border-solid border-neutral-500 shadow-md'}
+                           name={'content'}
+                           text={'text'}
+                           value={todo.content}
+                           onChange={handleChangeTodo}
+                    >
+                    </input>
+                </div>
+            </div>
+
+            {/* DUEDATE */}
+            <div className={'flex justify-center'}>
+                <div className={'relative mb-4 flex w-full flex-wrap items-stretch'}>
+                    <div className={'w-1/5 p-6 text-right font-bold'}>DUEDATE</div>
+                    <input className={'w-4/5 p-6 rounded-r borer border-solid border-neutral-500 shadow-md'}
+                           name={'dueDate'}
+                           type={'date'}
+                           value={todo.dueDate}
+                           onChange={handleChangeTodo}
+                    >
+                    </input>
+                </div>
+            </div>
+
+            {/* ADD BUTTON */}
+            <div className={'flex justify-end'}>
+                <div className={'relative mb-4 flex p-4 flex-wrap items-stretch'}>
+                    <button type={'button'}
+                            className={'rounded p-4 w-36 bg-blue-500 text-xl text-white'}
+                            onClick={handleClickAdd}
+                    >
+                        ADD
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default AddComponent;
