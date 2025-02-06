a3e50d669f03e236c24bec4ba82425210a6e67ca
jun <xodlf100@gmail.com>
2025-02-06 23:30:51 +0900

    feat Backend - file 조회 기능 구현

--- apiserver/src/main/java/org/zerock/apiserver/controller/ProductController.java
+++ apiserver/src/main/java/org/zerock/apiserver/controller/ProductController.java
@@ -2,9 +2,9 @@ package org.zerock.apiserver.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.zerock.apiserver.dto.ProductDTO;
 import org.zerock.apiserver.util.CustomFileUtil;
@@ -20,6 +20,7 @@ public class ProductController {
 
     private final CustomFileUtil fileUtil;
 
+    // 파일 등록
     @PostMapping("/")
     public Map<String, String> register(ProductDTO productDTO) {
 
@@ -32,5 +33,12 @@ public class ProductController {
         log.info("uploadedFileNames" + uploadedFileNames);
 
         return Map.of("RESULT", "SUCCESS");
+    }
+
+    // 파일 조회
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable("fileName") String fileName) {
+
+        return fileUtil.getFile(fileName);
     }
 }
--- apiserver/src/main/java/org/zerock/apiserver/util/CustomFileUtil.java
+++ apiserver/src/main/java/org/zerock/apiserver/util/CustomFileUtil.java
@@ -5,6 +5,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -40,6 +44,7 @@ public class CustomFileUtil {
         log.info("----------------------------------------");
     }
 
+    // 파일 등록
     public List<String> saveFiles(List<MultipartFile> files) throws RuntimeException {
         if (files == null || files.size() == 0) {
             return null;
@@ -71,4 +76,24 @@ public class CustomFileUtil {
 
         return uploadNames;
     }
+
+    // 파일 조회
+    public ResponseEntity<Resource> getFile(String fileName) {
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+        if (!resource.isReadable()) {
+            resource = new FileSystemResource(uploadPath + File.separator + "default.jpg");
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+
+        try {
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
 }
