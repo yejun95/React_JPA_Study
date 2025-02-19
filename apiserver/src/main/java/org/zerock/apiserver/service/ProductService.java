0790ccad58f487a0bff68439efff15680cca5b5e
jun <xodlf100@gmail.com>
2025-02-19 22:29:13 +0900

    feat Backend - 상품 등록 api 구현

--- apiserver/src/main/java/org/zerock/apiserver/service/ProductService.java
+++ apiserver/src/main/java/org/zerock/apiserver/service/ProductService.java
@@ -9,4 +9,6 @@ import org.zerock.apiserver.dto.ProductDTO;
 public interface ProductService {
 
     PageResponseDTO<ProductDTO> getList(PageRequestDTO pageRequestDTO);
+
+    Long register(ProductDTO productDTO);
 }
