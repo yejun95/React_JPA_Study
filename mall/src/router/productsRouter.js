8497c71e7053c0fd595352699413303ba1cbbb89
jun <xodlf100@gmail.com>
2025-02-25 00:03:06 +0900

    feat React - set productRouter and ListPage

--- mall/src/router/productsRouter.js
+++ mall/src/router/productsRouter.js
@@ -1,7 +1,19 @@
 import { Suspense, lazy } from "react";
+import {Navigate} from "react-router-dom";
+
+const Loading = <div>Loading....</div>
+const ProductList = lazy(() => import("../pages/products/ListPage"));
 
 const productsRouter = () => {
     return [
+        {
+            path: '',
+            element: <Navigate replace to={'/products/list'}/>
+        },
+        {
+            path: 'list',
+            element: <Suspense fallback={Loading}><ProductList/></Suspense>
+        }
 
     ]
 }
