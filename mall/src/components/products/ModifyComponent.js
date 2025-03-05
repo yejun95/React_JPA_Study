966ae895580971cd94d34306c45aafe42d582fb4
jun <xodlf100@gmail.com>
2025-03-05 22:00:24 +0900

    feat React - 상품 상세페이지 수정 구현

--- mall/src/components/products/ModifyComponent.js
+++ mall/src/components/products/ModifyComponent.js
@@ -1,7 +1,9 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {getOne} from "../../api/productsApi";
+import {getOne, putOne} from "../../api/productsApi";
 import FetchingModal from "../common/FetchingModal";
 import {API_SERVER_HOST} from "../../api/todoApi";
+import useCustomMove from "../../hooks/useCustomMove";
+import ResultModal from "../common/ResultModal";
 
 const initState = {
     pno: 0,
@@ -17,6 +19,8 @@ const host = API_SERVER_HOST;
 function ModifyComponent({pno}) {
     const [product, setProduct] = useState(initState);
     const [fetching, setFetching] = useState(false);
+    const [result, setResult] = useState(false);
+    const {moveToList, moveToRead} = useCustomMove();
 
     const uploadRef = useRef();
 
@@ -44,12 +48,50 @@ function ModifyComponent({pno}) {
         setProduct({...product});
     }
 
+    const handleClickModify = () => {
+        const files = uploadRef.current.files;
+        const formData  = new FormData();
+
+        //신규 파일 추가
+        for(let i = 0; i < files.length; i++) {
+            formData.append('files', files[i]);
+        }
+
+        formData.append('pname', product.pname);
+        formData.append('pdesc', product.pdesc);
+        formData.append('price', product.price);
+        formData.append('delFlag', product.delFlag);
+
+        //기존에 있는 파일 유지
+        for (let i = 0; i < product.uploadFileNames.length; i++) {
+            formData.append('uploadFileNames', product.uploadFileNames[i]);
+        }
+
+        setFetching(true);
+
+        putOne(pno, formData).then(data => {
+            setResult('Modified');
+            setFetching(false);
+        })
+
+    }
+
+    const closeModal = () => {
+        if (result === 'Modified') {
+            moveToRead(pno);
+        }
+        setResult(null);
+    }
+
     return (
         <div className={'border-2 border-sky-200 mt-10 m-2 p-4'}>
             {fetching ? <FetchingModal/> : <></>}
 
+            {result ? <ResultModal title={`${result}`} content={'처리되었습니다.'} callbackFn={closeModal}/> : <></>}
+
             <div className={'flex justify-center'}>
                 <div className={'relative mb-4 flex w-full flex-wrap items-stretch'}>
+                    <div className={'w-1/5 p-6 text-right font-bold'}>Product Name</div>
                     <input className={'w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md'}
                            name={'pname'}
                            type={'text'}
@@ -130,6 +172,25 @@ function ModifyComponent({pno}) {
                         )}
                     </div>
                 </div>
+
+            </div>
+            <div className={'flex justify-end p-4'}>
+                <button type={'button'}
+                        className={'rounded p-4 m-2 text-xl w-32 text-white bg-red-500'}
+                >
+                    Delete
+                </button>
+                <button type={'button'}
+                        className={'inline-block rounded p-4 m-2 text-xl w-32 text-white bg-orange-500'}
+                        onClick={handleClickModify}
+                >
+                    Modify
+                </button>
+                <button type={'button'}
+                        className={'rounded p-4 m-2 text-xl w-32 text-white bg-blue-500'}
+                >
+                    List
+                </button>
             </div>
 
         </div> //end container
