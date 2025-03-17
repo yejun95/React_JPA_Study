a1df5ea835b6b76e1fcc3d94d9dc52bd02d761cf
jun <xodlf100@gmail.com>
2025-03-17 22:30:51 +0900

    feat Backend - jwt 적용 - 1

--- apiserver/src/main/java/org/zerock/apiserver/security/handler/APILoginSuccessHandler.java
+++ apiserver/src/main/java/org/zerock/apiserver/security/handler/APILoginSuccessHandler.java
@@ -9,6 +9,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.zerock.apiserver.dto.MemberDTO;
+import org.zerock.apiserver.util.JWTUtil;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -27,8 +28,10 @@ public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
 
         Map<String, Object> claims = memberDTO.getClaims();
 
-        claims.put("accessToken", "");
-        claims.put("refreshToken", "");
+        String accessToken = JWTUtil.generateToken(claims, 10);
+        String refreshToken = JWTUtil.generateToken(claims, 60 * 24);
+        claims.put("accessToken", accessToken);
+        claims.put("refreshToken", refreshToken);
 
         Gson gson = new Gson();
         String jsonStr = gson.toJson(claims);
@@ -36,6 +39,7 @@ public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
         response.setContentType("application/json; charset=UTF-8");
 
         //로그인했을 때 유저 정보를 프론트로 넘기기 위함
+        //현재는 토큰 정보를 넘기지만 원래는 넘기면 안됨
         PrintWriter printWriter = response.getWriter();
         printWriter.println(jsonStr);
         printWriter.close();
---
+++ apiserver/src/main/java/org/zerock/apiserver/util/CustomJWTException.java
@@ -0,0 +1,8 @@
+package org.zerock.apiserver.util;
+
+public class CustomJWTException  extends RuntimeException {
+
+    public CustomJWTException(String msg) {
+        super(msg);
+    }
+}
