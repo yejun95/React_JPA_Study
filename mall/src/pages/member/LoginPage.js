48eec53dacce78f549b186817c5755e64863659f
jun <xodlf100@gmail.com>
2025-04-05 23:52:36 +0900

    feat React - add Login Page

---
+++ mall/src/pages/member/LoginPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import BasicMenu from "../../components/menus/BasicMenu";
+
+function LoginPage(props) {
+    return (
+        <div className={'fixed top-0 left-0 z-[1055] flex flex-col h-full w-full'}>
+            <BasicMenu/>
+
+            <div className={'flex flex-wrap w-full h-full justify-center items-center border-2'}>
+                <div className={'text-2xl'}>
+                    Login Page
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
