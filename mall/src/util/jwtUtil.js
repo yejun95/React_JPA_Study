1d1b62a0e76b05056e037d416231d0993aa094e4
jun <xodlf100@gmail.com>
2025-04-13 23:21:06 +0900

    feat React - refresh 토큰을 이용한 Access 토큰 자동 갱신 * Axios interceptors 활용

--- mall/src/util/jwtUtil.js
+++ mall/src/util/jwtUtil.js
@@ -1,8 +1,22 @@
 import axios from "axios";
-import {getCookie} from "./cookieUtil";
+import {getCookie, setCookie} from "./cookieUtil";
+import {API_SERVER_HOST} from "../api/todoApi";
 
 const jwtAxios = axios.create();
 
+const refreshJWT = async (accessToken, refreshToken) => {
+
+    const host = API_SERVER_HOST;
+
+    const header = {headers: {'Authorization': `Bearer ${accessToken}`}}
+
+    const res = await axios.get(`${host}/api/member/refresh?refreshToken=${refreshToken}`, header);
+
+    // 새로 받아온 refreshToken
+    console.log('res', res);
+
+    return res.data;
+}
 
 // before return request
 const beforeReq = (config) => {
@@ -41,6 +55,28 @@ const requestFail = (err) => {
 // before return response
 const beforeRes = async (res) => {
     console.log('before return response............');
+
+    const data = res.data;
+    console.log('data.error', data.error);
+    if (data && data.error === 'ERROR_ACCESS_TOKEN') {
+
+        const memberCookieValue = getCookie('member');
+
+        const result = await refreshJWT(memberCookieValue.accessToken, memberCookieValue.refreshToken);
+
+        // 새로운 accessToken과 refreshToken을 응답받는다.
+        memberCookieValue.accessToken = result.accessToken;
+        memberCookieValue.refreshToken = result.refreshToken;
+
+        setCookie('member', JSON.stringify(memberCookieValue), 1); // 어차피 refresh로 계속 토큰을 받을 수 있기 때문에 1로 설정
+
+        // 에러가 났었기 때문에 원래 하려 했던 작업을 재호출
+        const originalRequest = res.config;
+
+        originalRequest.headers.Authorization = `Bearer ${result.accessToken}`;
+
+        return axios(originalRequest);
+    }
 
     return res;
 }
