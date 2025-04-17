8acfb475b1de6cfdc6d4d885e2a2e9320ba839de
jun <xodlf100@gmail.com>
2025-04-17 23:57:37 +0900

    feat Backend - API 서버에서 소셜 회원 처리 * 이전 커밋은 커밋명이 잘못됨

--- apiserver/src/main/java/org/zerock/apiserver/service/MemberService.java
+++ apiserver/src/main/java/org/zerock/apiserver/service/MemberService.java
@@ -1,10 +1,26 @@
 package org.zerock.apiserver.service;
 
 import org.springframework.transaction.annotation.Transactional;
+import org.zerock.apiserver.domain.Member;
 import org.zerock.apiserver.dto.MemberDTO;
+
+import java.util.stream.Collectors;
 
 @Transactional
 public interface MemberService {
 
     MemberDTO getKakaoMember(String accessToken);
+
+    default MemberDTO entityToDTO(Member member) {
+
+        MemberDTO dto = new MemberDTO(
+                member.getEmail(),
+                member.getPw(),
+                member.getNickname(),
+                member.isSocial(),
+                member.getMemberRoleList().stream().map(memberRole -> memberRole.name()).collect(Collectors.toList())
+        );
+
+        return dto;
+    }
 }
--- apiserver/src/main/java/org/zerock/apiserver/service/MemberServiceImpl.java
+++ apiserver/src/main/java/org/zerock/apiserver/service/MemberServiceImpl.java
@@ -6,14 +6,18 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.zerock.apiserver.domain.Member;
+import org.zerock.apiserver.domain.MemberRole;
 import org.zerock.apiserver.dto.MemberDTO;
 import org.zerock.apiserver.repository.MemberRepository;
 
 import java.util.LinkedHashMap;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,19 +25,52 @@ import java.util.LinkedHashMap;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public MemberDTO getKakaoMember(String accessToken) {
 
-        // accessToken을 이용해서 사용자 정보 가져오기
-        getEmailFromKakaoAccessToken(accessToken);
+        // accessToken을 이용해서 사용자 정보 가져오기 (nickname == email)
+        String nickname = getEmailFromKakaoAccessToken(accessToken);
 
         // 기존 DB에 회원 정보가 있는 경우 / 없는 경우
+        Optional<Member> result = memberRepository.findById(nickname);
 
-        return null;
+        if (result.isPresent()) {
+
+            MemberDTO memberDTO = entityToDTO(result.get());
+
+            return memberDTO;
+        }
+
+        Member socialMember = makeSocialMember(nickname);
+
+        memberRepository.save(socialMember);
+
+        MemberDTO memberDTO = entityToDTO(socialMember);
+
+        return memberDTO;
     }
 
-    private void getEmailFromKakaoAccessToken(String accessToken) {
+    private Member makeSocialMember(String email) {
+        // 실제로 넘어오는 값은 nickname임
+
+        String tempPassword = makeTempPassword();
+        log.info("tempPassword: {}", tempPassword);
+
+        Member member = Member.builder()
+                .email(email)
+                .pw(passwordEncoder.encode(tempPassword))
+                .nickname("Social Member")
+                .social(true)
+                .build();
+
+        member.addRole(MemberRole.USER);
+
+        return member;
+     }
+
+    private String getEmailFromKakaoAccessToken(String accessToken) {
 
         String kakaoGetUserURL = "https://kapi.kakao.com/v2/user/me";
 
@@ -54,8 +91,22 @@ public class MemberServiceImpl implements MemberService {
 
         LinkedHashMap<String, LinkedHashMap> bodyMap = response.getBody();
 
-        log.info("---------------------------------------");
-        log.info("bodyMap: {}", bodyMap);
+        LinkedHashMap<String, String> kakaoAccount = bodyMap.get("properties");
+        log.info("kakaoAccount: {}", kakaoAccount);
 
+        String nickname = kakaoAccount.get("nickname");
+        log.info("nickname: {}", nickname);
+
+        return nickname;
+    }
+
+    private String makeTempPassword() {
+
+        StringBuffer buffer = new StringBuffer();
+
+        for (int i = 0; i < 10; i++) {
+            buffer.append((char) ((int) (Math.random() * 55) + 65));
+        }
+        return buffer.toString();
     }
 }
