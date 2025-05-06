807894d1b08ebd5100b73ddea7ebb0d8f726ea37
jun <xodlf100@gmail.com>
2025-05-07 00:35:49 +0900

    feat Backend CartController - 카트 아이템 변경 api 구현

---
+++ apiserver/src/main/java/org/zerock/apiserver/controller/CartController.java
@@ -0,0 +1,39 @@
+package org.zerock.apiserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.apiserver.dto.CartItemDTO;
+import org.zerock.apiserver.dto.CartItemListDTO;
+import org.zerock.apiserver.service.CartService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/api/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    // 로그인한 사용자와 security 값이 일치해야만 처리
+    @PreAuthorize("#itemDTO.email == authentication.name")
+    @PostMapping("/change")
+    public List<CartItemListDTO> changeCart(@RequestBody CartItemDTO itemDTO) {
+        log.info(itemDTO);
+
+        // 음수 처리
+        if (itemDTO.getQty() <= 0) {
+            return cartService.remove(itemDTO.getCino());
+        }
+
+        return cartService.addOrModify(itemDTO);
+    }
+
+
+}
