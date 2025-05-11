441ff2959a78c3f743f3ece1cf5422e09eb2a9ee
jun <xodlf100@gmail.com>
2025-05-11 23:57:41 +0900

    feat BE - CartItemListDTO : pno 추가

--- apiserver/src/main/java/org/zerock/apiserver/dto/CartItemListDTO.java
+++ apiserver/src/main/java/org/zerock/apiserver/dto/CartItemListDTO.java
@@ -13,15 +13,18 @@ public class CartItemListDTO {
 
     private int qty;
 
+    private Long pno;
+
     private String pname;
 
     private int price;
 
     private String imageFile;
 
-    public CartItemListDTO(Long cino, int qty, String pname, int price, String imageFile) {
+    public CartItemListDTO(Long cino, int qty, Long pno, String pname, int price, String imageFile) {
         this.cino = cino;
         this.qty = qty;
+        this.pno = pno;
         this.pname = pname;
         this.price = price;
         this.imageFile = imageFile;
