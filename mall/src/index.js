00edc5dc01f75c853872e6a4e3945178ae400723
jun <xodlf100@gmail.com>
2025-05-11 21:51:24 +0900

    feat FE - add tanStack Query (=React Query)

--- mall/src/index.js
+++ mall/src/index.js
@@ -5,11 +5,20 @@ import App from './App';
 import {Provider} from "react-redux";
 import store from "./store";
 
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
+<QueryClientProvider client={queryClient}>
     <Provider store={store}>
         <App />
     </Provider>
+    <ReactQueryDevtools initialIsOpen={true}/>
+</QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
