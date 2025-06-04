6f955b7f7773063327cbf37b201d930c601b0621
jun <xodlf100@gmail.com>
2025-06-04 23:23:24 +0900

    feat typeFE - useMutation: mutate를 통한 api 호출

--- frontend_typescript/src/components/products/addComponent.tsx
+++ frontend_typescript/src/components/products/addComponent.tsx
@@ -4,6 +4,7 @@ import PendingModal from "../common/pendingModal"
 import ResultModal from "../common/resultModal"
 import useCustomMove from "../../hooks/useCustomMove"
 import jwtAxios from "../../util/jwtUtil"
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 
 interface ProductAddResult {
   result : number,
@@ -16,28 +17,50 @@ const initState: ProductAddResult = {
 }
 
 //액션처리 함수 
-const addAsyncAction = async (state:ProductAddResult, formData:FormData ) => {
+// const addAsyncAction = async (state:ProductAddResult, formData:FormData ) => {
+//
+//   console.log("addAsyncAction........")
+//
+//   await new Promise(resolve => setTimeout(resolve, 2000))
+//
+//   const pname = formData.get("pname") as string
+//   if(!pname) {
+//     return { result:0, error: "Insert Product Name"}
+//   }
+//   const res = await jwtAxios.post('http://localhost:8080/api/products/', formData )
+//
+//   return {result: res.data.result}
+//
+// }
 
-  console.log("addAsyncAction........")
-
-  await new Promise(resolve => setTimeout(resolve, 2000))
-
-  const pname = formData.get("pname") as string 
-  if(!pname) {
-    return { result:0, error: "Insert Product Name"}
-  }
-  const res = await jwtAxios.post('http://localhost:8080/api/products/', formData )
-  
-  return {result: res.data.result}
-
+const addProduct = async (formData: FormData) => {
+  const res = await jwtAxios.post(`http://localhost:8080/api/products`, formData)
+  return { result: res.data.result }
 }
   
 
 function AddComponent() {
 
-  const [state, action ,isPending] = useActionState(addAsyncAction, initState)
+  // const [state, action ,isPending] = useActionState(addAsyncAction, initState)
 
   const {moveToList} = useCustomMove()
+
+  const queryClient = useQueryClient()
+  const mutation = useMutation({
+    mutationFn: addProduct,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ['products/list'],
+        exact: false
+      })
+    }
+  })
+
+  const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    mutation.mutate(formData)
+  }
 
   const closeModal = () => {
 
@@ -46,17 +69,17 @@ function AddComponent() {
 
   return ( 
   <div className = "border-2 border-sky-200 mt-10 m-2 p-4">
-    {isPending && <PendingModal/>}
+    {mutation.isPending && <PendingModal/>}
     
-    {state.result !== 0 && 
+    {mutation.data?.result &&
     <ResultModal 
       title="상품 추가 결과" 
-      content={`새로운 ${state.result} 상품 추가됨`} 
+      content={`새로운 ${mutation.data.result} 상품 추가됨`}
       callbackFn={closeModal}/>
     } 
     
     <h1>Add Component</h1>
-    <form action={action}>
+    <form onSubmit={handleSubmit}>
 
       <div className="flex justify-center">
       <div className="relative mb-4 flex w-full flex-wrap items-stretch">
--- frontend_typescript/src/pages/products/readPage.tsx
+++ frontend_typescript/src/pages/products/readPage.tsx
@@ -1,24 +1,41 @@
 
-import { useLoaderData } from "react-router";
+import {useLoaderData, useParams} from "react-router";
 import { LoaderFunctionArgs } from "react-router";
 import ReadComponent from "../../components/products/readComponent";
 import jwtAxios from "../../util/jwtUtil";
+import {useQuery} from "@tanstack/react-query";
+import PendingModal from "../../components/common/pendingModal.tsx";
 
-export async function loadProduct({params}: LoaderFunctionArgs) {
-  const {pno} = params
-  const res = await jwtAxios.get(`http://localhost:8080/api/products/${pno}`)
-  return res.data
-}
+// export async function loadProduct({params}: LoaderFunctionArgs) {
+//   const {pno} = params
+//   const res = await jwtAxios.get(`http://localhost:8080/api/products/${pno}`)
+//   return res.data
+// }
 
 
 function ReadPage() {
+  // const product:ProductDTO = useLoaderData()
 
-  const product:ProductDTO = useLoaderData()
+  const { pno } = useParams()
+
+  const {data, isPending, error} = useQuery({
+    queryKey: ['products', pno], // 캐싱 이름
+    queryFn: async () => { // 반환해줄 함수
+      const res = await jwtAxios.get(`http://localhost:8080/api/products/${pno}`)
+      return res.data
+    },
+    staleTime: 1000 * 60 * 24 // api 신선도 유지 시간
+  })
+
+  const product = data
 
   return (  
     <div className="w-full">   
       <div>Product Read</div>
-      <ReadComponent product={product}></ReadComponent>
+      {isPending && <PendingModal/>}
+      {product &&
+        <ReadComponent product={product}></ReadComponent>
+      }
   </div>
 
   );
