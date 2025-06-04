b931f3a9c657d2b57a251901b0353dda917a8f4d
jun <xodlf100@gmail.com>
2025-06-04 23:39:27 +0900

    feat typeFE - modify mutation 적용

--- frontend_typescript/src/components/member/modifyComponent.tsx
+++ frontend_typescript/src/components/member/modifyComponent.tsx
@@ -1,8 +1,8 @@
-import { useActionState } from "react"
-import useCustomLogin from "../../hooks/useCustomLogin"
-import jwtAxios from "../../util/jwtUtil"
-import PendingModal from "../common/pendingModal"
-import ResultModal from "../common/resultModal"
+import { useState } from "react";
+import useCustomMove from "../../hooks/useCustomMove";
+import ResultModal from "../common/resultModal";
+import jwtAxios from "../../util/jwtUtil";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 interface ModifyResult {
    result: string,
@@ -13,90 +13,172 @@ const initState: ModifyResult = {
    error: ''
 }
 
-const modifyAction = async ( state:ModifyResult, formData:FormData) => {
-   const email = formData.get("email") as string
-   const pw = formData.get("pw") as string
-   const nickname = formData.get("nickname") as string 
-   
-   if (pw.length < 8) {
-    return { result: '', error: '패스워드는 8자 이상이어야 합니다.' };
-   }
-   try {
-    
-    await jwtAxios.put('http://localhost:8080/api/member/modify', { email, pw, nickname });
-    
-   } catch (err: any) {
-    return { result: '', error: err.response?.data?.message || '수정 중 오류가 발생했습니다.' };
-   }
-  return {result:'Modified', error:''}
+// const modifyAction = async ( state:ModifyResult, formData:FormData) => {
+//    const email = formData.get("email") as string
+//    const pw = formData.get("pw") as string
+//    const nickname = formData.get("nickname") as string
+//
+//    if (pw.length < 8) {
+//     return { result: '', error: '패스워드는 8자 이상이어야 합니다.' };
+//    }
+//    try {
+//
+//     await jwtAxios.put('http://localhost:8080/api/member/modify', { email, pw, nickname });
+//
+//    } catch (err: any) {
+//     return { result: '', error: err.response?.data?.message || '수정 중 오류가 발생했습니다.' };
+//    }
+//   return {result:'Modified', error:''}
+// }
+
+
+function ModifyComponent( {product}: {product:ProductDTO}) {
+  const {moveToRead, moveToList} = useCustomMove()
+  const [images, setImages] = useState<string[]>([...product.uploadFileNames]);
+
+  const deleteOldImages = (event: React.MouseEvent<HTMLButtonElement> , target: string) => {
+    event.preventDefault()
+    event.stopPropagation()
+    setImages(prev => prev.filter(img => img !== target));
+  };
+
+  const queryClient = useQueryClient()
+
+  const deleteMutation = useMutation({
+    mutationFn: async () => {
+      const res = await jwtAxios.delete(`http://localhost:8080/api/products/${product.pno}`)
+      return res.data
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries( {queryKey: ['product', String(product.pno) ]})
+      queryClient.invalidateQueries( {queryKey: ['products/list'], exact: false})
+    }
+  })
+
+  const modifyMutation = useMutation({
+    mutationFn: async (formData:FormData) => {
+      const header = {headers: {"Content-Type": "multipart/form-data"}}
+      const res = await jwtAxios.put(`http://localhost:8080/api/products/${product.pno}`, formData, header)
+
+      return res.data
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries( {queryKey: ['product', String(product.pno) ]})
+      queryClient.invalidateQueries( {queryKey: ['products/list'], exact: false})
+    }
+  })
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const submitter = (e.nativeEvent as SubmitEvent).submitter as HTMLButtonElement
+    const actionType = submitter.value
+    console.log("actionType", actionType)
+
+    if(actionType === 'modify'){
+      modifyMutation.mutate(formData)
+
+    } else if(actionType === 'delete') {
+      deleteMutation.mutate()
+    }
+
+  }
+  return (
+    <div className = "border-2 border-sky-200 mt-10 m-2 p-4 bg-white">
+      { (deleteMutation.data || modifyMutation.data) && <ResultModal title="처리완료" content="처리 완료" callbackFn={() => {
+        console.log(deleteMutation.data)
+
+        if(modifyMutation.data?.RESULT ==='SUCCESS'){
+          moveToRead(product.pno)
+        }
+        if(deleteMutation.data?.RESULT === 'SUCCESS') {
+          moveToList()
+        }
+      }} />}
+      <form onSubmit={handleSubmit} >
+        <div className="flex justify-center mt-10">
+          <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+            <div className="w-1/5 p-6 text-right font-bold">PNO</div>
+            <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+                   name="pno" required defaultValue={product.pno}>
+            </input>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+            <div className="w-1/5 p-6 text-right font-bold">PNAME</div>
+            <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+                   name="pname" required defaultValue={product.pname}>
+            </input>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+            <div className="w-1/5 p-6 text-right font-bold">PRICE</div>
+            <input
+              className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+              name="price" type={'number'} defaultValue={product.price}>
+            </input>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+            <div className="w-1/5 p-6 text-right font-bold">PDESC</div>
+            <textarea
+              className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md resize-y"
+              name="pdesc" rows={4} required defaultValue={product.pdesc}>
+    </textarea>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+            <div className="w-1/5 p-6 text-right font-bold">Files</div>
+            <input
+              className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+              type={'file'}
+              name="files"
+              multiple={true}>
+            </input>
+          </div>
+        </div>
+        <div className="w-full justify-center flex flex-col m-auto items-center">
+          {images.map( (imgFile, i) =>
+            <div
+              className="flex justify-center flex-col w-1/3"
+              key = {i}>
+              <button className="bg-blue-500 text-3xl text-white" onClick={(event) => deleteOldImages(event, imgFile)}>DELETE</button>
+              <img
+                alt ="img"
+                src={`http://localhost:8080/api/products/view/s_${imgFile}`}/>
+              <input type="hidden" name="uploadFileNames" value={imgFile}/>
+
+            </div>
+          )}
+        </div>
+
+        <div className="flex justify-end p-4">
+          <button type="submit" name='actionType' value='delete'
+                  className="rounded p-4 m-2 text-xl w-32 text-white bg-red-500">
+            Delete
+          </button>
+          <button type="submit" name='actionType' value='modify'
+                  className="inline-block rounded p-4 m-2 text-xl w-32 text-white bg-orange-500">
+            Modify
+          </button>
+
+          <button type="button"
+                  className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500"
+                  onClick={() => moveToList()}
+          >
+            List
+          </button>
+        </div>
+      </form>
+    </div>
+  )
 }
+export default ModifyComponent;
 
-
-function ModifyComponent() {
-
-  const {loginState, moveToLogin} = useCustomLogin()
-
-  const [state, action, isPending ] = useActionState(modifyAction, initState)
-    const closeModal = () => {
-      moveToLogin()
-    }
-   
-    return (
-     <div className="mt-6">
-     {isPending && <PendingModal/>}  
-     {state.error && <div className="bg-amber-500"> {state.error} </div>}
-     {state.result &&  
-    <ResultModal 
-    title="회원 정보 수정 결과 " 
-    content={`회원 정보가 수정되었습니다.`} 
-    callbackFn={closeModal}/>
-    }
-      <form action={action}>
-     <div className="flex justify-center">
-      <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-       <div className="w-1/5 p-6 text-right font-bold">Email</div>
-       <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md" 
-       name="email"
-       type={'text'}
-       defaultValue={loginState.email} 
-       readOnly
-       >
-       </input>
-       </div>
-     </div>
-  <div className="flex justify-center">
-    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-     <div className="w-1/5 p-6 text-right font-bold">Password</div>
-     <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md" 
-     name="pw"
-     type={'password'} 
-     >
-     </input>
-     </div>
-   </div>
-   <div className="flex justify-center">
-    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
-     <div className="w-1/5 p-6 text-right font-bold">Nickname</div>
-     <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md" 
-     name="nickname"
-     type={'text'} 
-     defaultValue={loginState.nickname}
-     >
-     </input>
-    </div>
-   </div>
-   <div className="flex justify-center">
-    <div className="relative mb-4 flex w-full flex-wrap justify-end">
-     <button type="submit" 
-      className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500"
-     >
-      Modify
-     </button> 
-    </div>
-   </div>
-  </form> 
- </div>
-   )
- } 
- export default ModifyComponent
   
--- frontend_typescript/src/pages/member/modifyPage.tsx
+++ frontend_typescript/src/pages/member/modifyPage.tsx
@@ -1,25 +1,42 @@
-
 import ModifyComponent from "../../components/member/modifyComponent"
 import BasicMenu from "../../components/menus/basicMenu"
+import {useParams} from "react-router";
+import {useQuery} from "@tanstack/react-query";
+import jwtAxios from "../../util/jwtUtil.ts";
+
 function ModifyPage () {
-return (
-    <>
-    <BasicMenu/>
-    <div className="bg-white my-5 w-full flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">   
-     
-     <main className="bg-sky-300 md:w-4/5 lg:w-3/4 px-5 py-5">
-      <div className=" text-3xl">Member Modify Page</div>
-      <div className="bg-white w-full mt-4 p-2">
-        <ModifyComponent/>
-      </div>  
-      </main>   
-    
-    <aside className="bg-green-300 md:w-1/3 lg:w-1/4 px-5 py-40">    
-     <h1 className="text-2xl md:text-4xl"> Sidebar </h1> 
-    </aside> 
-   
-   </div>
-  </>
-  )
+  const { pno } = useParams()
+
+  const { data, isPending, error } = useQuery({
+    queryKey: ['product', pno],
+    queryFn: async () => {
+      const res = await jwtAxios.get(`http://localhost:8080/api/products/${pno}`)
+      return res.data
+    },
+    staleTime: 1000 * 60 * 24
+  })
+
+  const product: ProductDTO = data
+
+
+  return (
+      <>
+      <BasicMenu/>
+      <div className="bg-white my-5 w-full flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+
+       <main className="bg-sky-300 md:w-4/5 lg:w-3/4 px-5 py-5">
+        <div className=" text-3xl">Member Modify Page</div>
+        <div className="bg-white w-full mt-4 p-2">
+          <ModifyComponent/>
+        </div>
+        </main>
+
+      <aside className="bg-green-300 md:w-1/3 lg:w-1/4 px-5 py-40">
+       <h1 className="text-2xl md:text-4xl"> Sidebar </h1>
+      </aside>
+
+     </div>
+    </>
+    )
 }
 export default ModifyPage
